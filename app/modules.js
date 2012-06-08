b74bdc34f925144f934ac7d62815ad72317b2aec
Kevn Roberts <kev@thebrowntrout.net>
2012-06-07 20:20:52 -0600

    passed the modules test

--- app/modules.js
+++ app/modules.js
@@ -2,8 +2,14 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
   return {
-    createModule : function() {
-
+    createModule : function(greeting, name) {
+    	return {
+    		name: name,
+    		greeting: greeting,
+    		sayIt: function(){
+    			return this.greeting + ", " + this.name;
+    		}
+    	};
     }
   };
 });
