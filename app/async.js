59a3fb8c45fbbb5dcbffd409ab6a4b2e9edea975
Kevn Roberts <kev@thebrowntrout.net>
2012-06-17 21:41:02 -0600

    passed the async tests (although could do better with manipulateRemoteData).

--- app/async.js
+++ app/async.js
@@ -1,11 +1,27 @@
 define(function() {
   return {
     async : function() {
-
+    	return {
+    		then: function(func){
+    			func(true);
+    		}
+    	};
     },
 
-    manipulateRemoteData : function() {
-
+    manipulateRemoteData : function(url) {
+		return {
+			then: function(func){
+				jQuery.get(url, function(data){
+		    		var i;
+		    		var result = []
+		    		for(i = 0; i < data.people.length; i++){
+		    			result.push(data.people[i].name);
+		    		}
+		    		result.sort();
+    				func(result);
+		    	});
+			}
+		};
     }
   };
 });
