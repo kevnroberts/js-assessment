591e255c8e6cf6f8057ce7cd8c51fe10c86b4445
kevnroberts <kev@thebrowntrout.net>
2013-07-12 14:58:24 -0600

    finally figured out the binary operations from numbers.js

--- app/numbers.js
+++ app/numbers.js
@@ -3,15 +3,21 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+        return 1 & (num >> (bit - 1));
     },
 
     base10: function(str) {
-
+        return parseInt(str, 2);
     },
 
     convertToBinary: function(num) {
+        var arr = [];
 
+        for(var i = 8; i > 0; i--){
+            arr.push(this.valueAtBit(num, i));
+        }
+
+        return arr.join('');
     },
 
     multiply: function(a, b) {
